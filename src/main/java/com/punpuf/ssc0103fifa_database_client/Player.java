84dfcfbbd2bba956d7316b643a0a9fef28a8753b
Marcus <punpuf@gmail.com>
2024-06-06 17:24:35 -0300

    Added Player class.

---
+++ src/main/java/com/punpuf/ssc0103fifa_database_client/Player.java
@@ -0,0 +1,63 @@
+package com.punpuf.ssc0103fifa_database_client;
+
+/**
+ * Esta classe representa as características de um jogador FIFA.
+ * @author Jean Patrick Ngandu Mamani
+ * @author Marcus da Silva Carr
+ */
+public class Player {
+    int id;
+    int age;
+    String nomeJogador;
+    String nacionalidade;
+    String nomeClube;
+    
+    public Player(int id, int age, String nomeJogador, String nacionalidade, String nomeClube) {
+        this.id = id;
+        this.age = age;
+        this.nomeJogador = nomeJogador;
+        this.nacionalidade = nacionalidade;
+        this.nomeClube = nomeClube;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNomeJogador() {
+        return nomeJogador;
+    }
+
+    public void setNomeJogador(String nomeJogador) {
+        this.nomeJogador = nomeJogador;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getNomeClube() {
+        return nomeClube;
+    }
+
+    public void setNomeClube(String nomeClube) {
+        this.nomeClube = nomeClube;
+    }
+}
+
