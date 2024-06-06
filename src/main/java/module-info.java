30bea2d6b5e01287b40b9f29a74bed4eb21e3867
Marcus <punpuf@gmail.com>
2024-06-06 17:21:43 -0300

    Configured project to include JavaFX.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.punpuf.ssc0103fifa_database_client {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.punpuf.ssc0103fifa_database_client to javafx.fxml;
+    exports com.punpuf.ssc0103fifa_database_client;
+}
